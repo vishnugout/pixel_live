bc6755a8035824717f662dc92a39dc508624053b
victor <noah@ihnetworks.com>
2023-12-07 13:21:16 +0000

    web-access_sg created

--- main.tf
+++ main.tf
@@ -9,3 +9,38 @@ resource "aws_key_pair" "auth_key" {
   }
 }
 
+
+
+resource "aws_security_group" "http_access" {
+  name        = "${var.project_name}-${var.project_env}-http-access"
+  description = "${var.project_name}-${var.project_env}-http-access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-http-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
+
