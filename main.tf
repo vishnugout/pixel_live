bf5acf332d31dd9fa53fa9f3077f30fd01eca1d6
victor <noah@ihnetworks.com>
2023-12-08 16:52:07 +0000

    eip added

--- main.tf
+++ main.tf
@@ -94,13 +94,24 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_eip" "frontend" {
 
+  instance = aws_instance.frontend.id
+  domain   = "vpc"
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-frontend"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
 
 resource "aws_route53_record" "frontend" {
   zone_id = data.aws_route53_zone.official.zone_id
   name    = "${var.hostname}.${var.hosted_zone_name}"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.frontend.public_ip]
+  records = [aws_eip.frontend.public_ip]
 }
 
